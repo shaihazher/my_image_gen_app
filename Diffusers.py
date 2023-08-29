92b06b9f6bd2b523bbefaee6ea8ec481e1eecfc0
shaihazher <96671723+shaihazher@users.noreply.github.com>
2023-08-29 15:00:45 +0530

    Add files via upload

---
+++ Diffusers.py
@@ -0,0 +1,70 @@
+import torch
+from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionXLPipeline
+from diffusers import DDIMScheduler, EulerAncestralDiscreteScheduler, DPMSolverMultistepScheduler
+import runpod
+import subprocess
+import nest_asyncio
+import base64
+from PIL import Image
+from io import BytesIO
+#nest_asyncio.apply()
+
+def gen_image(jobs):
+    job_input = jobs["input"]
+    
+    prompt = job_input["prompt"]
+    negative_prompt = ""
+
+    #nprompt = """UnrealisticDream {neg}"""
+    nprompt = "ugly, deformed, noisy, blurry, distorted, grainy"
+    pprompt = """Portrait photo of (Breathtaking) wet and (curvy) Kerala woman with (plump big belly:1.2) in rainy beach, 
+    wearing (wet) saree, 
+    {pos}, with cleavage and (curvy fat hips) exposed"""
+    negative_prompt = nprompt.format(neg = negative_prompt)
+    prompt = pprompt.format(pos = prompt)
+    pipe = StableDiffusionXLPipeline.from_single_file("epicrealism_naturalSinRC1VAE.safetensors", 
+                                                   safety_checker = None, requires_safety_checker = False, 
+                                                   use_safetensors=True, custom_pipeline="lpw_stable_diffusion_xl")
+    #pipe.unet.load_attn_procs(lora_path)
+    pipe = pipe.to("mps")
+    pipe.enable_attention_slicing()
+
+    pipe.load_textual_inversion("UnrealisticDream.pt")
+    #pipe.scheduler.config["use_karras_sigmas"] = True
+    pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    pipe.safety_checker = None
+    pipe.requires_safety_checker = False
+
+        #protovision - 896x1152 and nightvision 832x1216
+    image = pipe(prompt= prompt, negative_prompt=negative_prompt, num_images_per_prompt = 1, 
+                 guidance_scale = 3, height = 1216, width = 832, num_inference_steps=35).images
+    
+    image[0].save(str(job_input["id"])+".png")
+    img_name = str(job_input["id"])+".png"
+
+    cmd = ['python', 'GFPGAN/inference_gfpgan.py', '-i', img_name, '-o', 'results', '-v', '1.3', '-s', '2']
+
+    try:
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Oops! Something went wrong: {e}")
+
+    # Open image file
+    with open("results/restored_imgs"+img_name, "rb") as image_file:
+        # Read the file
+        image_data = image_file.read()
+
+        # Encode as base64
+        base64_image = base64.b64encode(image_data).decode("utf-8")
+
+    # Now, `base64_image` contains the base64 string representation of your image
+
+
+    return base64_image
+
+runpod.serverless.start({"handler": gen_image})
+
+
+
+
---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Include Python
+FROM python:3.11.1-buster
+
+# Define your working directory
+WORKDIR /
+
+# Install wget
+RUN apt-get update && apt-get install -y wget
+
+# Copy requirements.txt and install dependencies
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+# Copy all other files and directories
+COPY . .
+
+RUN wget -O /epicrealism_naturalSinRC1VAE.safetensors https://drive.google.com/file/d/12Qhp1wNRlhORZ2JO_HliL5pP1w-WNzpQ/view?usp=drive_link
+
+RUN wget -O /GFPGAN/weights/detection_Resnet50_Final.pth https://drive.google.com/file/d/1pQXZl-0qLDCy1hC0wVvvEEwJtPtr-ODu/view?usp=drive_link
+
+RUN wget -O /GFPGAN/weights/parsing_parsenet.pth https://drive.google.com/file/d/1_vUNbqM5v6cbPVVh8KUCKeD3xTdtaPdv/view?usp=drive_link
+
+RUN wget -O /GFPGAN/experiments/pretrained_models/GFPGANv1.3.pth https://drive.google.com/file/d/1_vUNbqM5v6cbPVVh8KUCKeD3xTdtaPdv/view?usp=drive_link
+
+RUN wget -O /GFPGAN/gfpgan/weights/GFPGANv1.3.pth https://drive.google.com/file/d/1_vUNbqM5v6cbPVVh8KUCKeD3xTdtaPdv/view?usp=drive_link
+
+# Call your file when your container starts
+CMD [ "python", "-u", "Diffusers.py" ]
